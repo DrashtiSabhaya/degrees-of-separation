07fd3d08b744dc0c920f62d0527292f2eb37ad44
Drashti <sabhayadrashti@gmail.com>
2023-01-28 09:20:45 -0800

    - Setup file fixes

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBweyO2SLopth9C6sESDVKcTHORhvaIn6E",
-  authDomain: "degrees-of-sepration.firebaseapp.com",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: "degrees-of-sepration",
   storageBucket: "degrees-of-sepration.appspot.com",
-  messagingSenderId: "861082664106",
-  appId: "1:861082664106:web:f29e9e7c89aa4ec08424d3",
-  measurementId: "G-Y57X6W1VBV",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
